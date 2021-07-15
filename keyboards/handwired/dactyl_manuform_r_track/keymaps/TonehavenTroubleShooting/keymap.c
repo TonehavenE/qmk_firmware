7cbe2b1046aec5c22dbf8004af6ab79c224ac704
TonehavenE <tonehaven@outlook.com>
2021-07-15 14:45:59 -0400

    Slight Keymap Change; shift is used more than ctrl, so offload space cadet to ctrl

--- keyboards/handwired/dactyl_manuform_r_track/keymaps/TonehavenTroubleShooting/keymap.c
+++ keyboards/handwired/dactyl_manuform_r_track/keymaps/TonehavenTroubleShooting/keymap.c
@@ -223,8 +223,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_5x6(
 KC_GESC         , KC_1 ,   KC_2 , KC_3, KC_4, KC_5   ,               KC_6,    KC_7, KC_8   , KC_9  , KC_0        , KC_EQUAL  ,
 LALT_T(KC_MINUS), KC_Q ,   KC_W , KC_F, KC_P, KC_G   ,			     KC_J,    KC_L, KC_U   , KC_Y  , KC_QUOTE    , KC_SCOLON ,
-KC_LSPO         , KC_A ,   KC_R , KC_S, KC_T, KC_D   ,               KC_H,    KC_N, KC_E   , KC_I  , KC_O        , KC_RSPC   ,
-KC_LCTL         , KC_Z ,   KC_X , KC_C, KC_V, KC_B   ,               KC_K,    KC_M, KC_COMM, KC_DOT, KC_SLASH    , LGUI_T(KC_MINUS),
+KC_LSFT         , KC_A ,   KC_R , KC_S, KC_T, KC_D   ,               KC_H,    KC_N, KC_E   , KC_I  , KC_O        , KC_RSFT   ,
+KC_LCPO         , KC_Z ,   KC_X , KC_C, KC_V, KC_B   ,               KC_K,    KC_M, KC_COMM, KC_DOT, KC_SLASH    , KC_RCPC   ,
                            LGUI_T(KC_LBRC),RGUI_T(KC_RBRC),                                         TG(_ADJUST), TG(_LOWER),
                                        LT(_UNICODE, KC_TAB), LT(_RAISE, KC_SPACE),  _______, KC_BSPC,
                                        LT(_MATH, KC_EQUAL) , _______,      _______, LT(_FUNCTION, KC_ENT),
