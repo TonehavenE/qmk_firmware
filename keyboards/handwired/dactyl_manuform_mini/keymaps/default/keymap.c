6b8e531a8e391e4eb84a68262c789ff258b7368a
tonehaven <Tonehaven@outlook.com>
2021-11-04 10:26:44 -0400

    Slight changes to dacman_mini keymap.c

--- keyboards/handwired/dactyl_manuform_mini/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_mini/keymaps/default/keymap.c
@@ -9,22 +9,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0   , KC_EQUAL,
+        KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0   , KC_EQUAL,
         KC_LALT, KC_Q  , KC_W  , KC_F  , KC_P  , KC_G  ,                         KC_J  , KC_L  , KC_U  , KC_Y  , KC_QUOT, KC_SCLN,
         KC_LSFT, KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                         KC_H  , KC_N  , KC_E  , KC_I  , KC_O   , KC_RSFT,
         KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_K  , KC_M  ,KC_COMM,KC_DOT , KC_SLSH, KC_MINUS,
                          KC_LBRC,KC_RBRC,                                                       KC_LPRN, KC_RPRN,
                                          LOWER,                                         KC_BSPC,
-                                         KC_GRV, KC_SPC,                        KC_ENT,  RAISE,
-                                         KC_TAB, KC_LGUI,                        KC_DEL,KC_BSLASH
+                                         KC_TAB, KC_SPC,                        KC_ENT,  RAISE,
+                                         KC_LGUI, KC_TAB,                        KC_DEL,KC_BSLASH
     ),
 
     [_LOWER] = LAYOUT_5x6(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_HOME , KC_UP , KC_END ,_______,KC_PLUS,
-        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_LEFT , KC_DOWN , KC_RIGHT ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                        _______,KC_PSCR,                                                        _______, KC_P0,
+        _______,_______,KC_7, KC_8, KC_9,KC_LBRC,                        KC_RBRC, KC_HOME , KC_UP , KC_END ,_______,KC_PLUS,
+        _______,KC_HOME,KC_4, KC_5, KC_6 ,KC_LPRN,                        KC_RPRN, KC_LEFT , KC_DOWN , KC_RIGHT ,KC_MINS,KC_PIPE,
+        _______,_______,KC_1, KC_2, KC_3,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                        KC_0, KC_0,                                                        _______, KC_P0,
                                                 _______,                            _______,
                                                 _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______
