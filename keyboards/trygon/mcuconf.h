7ac1c48e98c7a4b0dd7a688f2b691237b587a64d
TonehavenE <tonehaven@outlook.com>
2022-05-14 09:11:17 -0400

    Getting started with trygon/olympus firmware

---
+++ keyboards/trygon/mcuconf.h
@@ -0,0 +1,16 @@
+// Copyright 2022 Eben Quennevile (@TonehavenE)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PLLM_VALUE
+#undef STM32_PLLN_VALUE
+#undef STM32_PLLP_VALUE
+#undef STM32_PLLQ_VALUE
+
+#define STM32_PLLM_VALUE    (STM32_HSECLK/1000000)
+#define STM32_PLLN_VALUE    192
+#define STM32_PLLP_VALUE    4
+#define STM32_PLLQ_VALUE    4
