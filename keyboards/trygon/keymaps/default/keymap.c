825a5b9812be15a8e0be4ac0167c6896c1dd16ac
TonehavenE <tonehaven@outlook.com>
2022-05-14 21:20:12 -0400

    Adding keymap layers

--- keyboards/trygon/keymaps/default/keymap.c
+++ keyboards/trygon/keymaps/default/keymap.c
@@ -1,24 +1,40 @@
 #include QMK_KEYBOARD_H
 
+#define _COLEMAK 0
+#define _GAME 1
+#define _RAISE 2
+#define _FUNC 3
+
+#define RAISE MO(_RAISE)
+#define LT_FUNC LT(_FUNC, KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │   │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │   │
-     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
-     */
-    [0] = LAYOUT_ortho_5x12(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-                 KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K
-    )
+    [_COLEMAK] = LAYOUT_ortho_5x12(
+        KC_GESC, KC_1,    KC_2,    KC_3 ,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_EQL,
+        KC_LALT, KC_Q,    KC_W,    KC_F ,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,   KC_SCLN,
+        KC_LSFT, KC_A,    KC_R,    KC_S ,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,      KC_RSFT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C ,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_MINS,
+                 KC_LPRN, KC_RPRN, RAISE,    KC_SPC,  KC_TAB,  KC_LGUI, LT_FUNC, KC_BSPC, KC_LBRC, KC_RBRC
+    ),
+    [_GAME] = LAYOUT_ortho_5x12(
+        KC_ESC , KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0   , KC_EQL,
+        KC_TAB , KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_J,    KC_L,    KC_U,    KC_Y,   KC_QUOT, KC_SCLN,
+        KC_LSFT, KC_G,    KC_A,    KC_S,    KC_D,    KC_F,    KC_H,    KC_N,    KC_E,    KC_I,   KC_O   , KC_RSFT,
+        KC_LCTL, KC_B,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_K,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+                 KC_F3,   KC_F12,  KC_LALT, KC_SPC,  MO(_FUNC), _______, _______, _______, TG(_GAME), TG(_GAME)
+    ),
+    [_RAISE] = LAYOUT_ortho_5x12(
+        KC_GRAVE, _______,    _______,  KC_0   ,  _______,  RESET  ,        RESET  ,  KC_DEL ,  _______,  _______  ,  _______  , _______,
+        _______ , _______,    KC_7   ,  KC_8   ,  KC_9   ,  _______,        _______,  KC_HOME,  KC_UP  ,  KC_END   ,  _______  , _______,
+        _______ , KC_0   ,    KC_4   ,  KC_5   ,  KC_6   ,  _______,        _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT ,  _______  , _______,
+        _______ , _______,    KC_1   ,  KC_2   ,  KC_3   ,  _______,        _______,  _______,  _______,  _______  ,  _______  , _______,
+                  _______,    _______,  _______,  _______,  _______,        _______,  _______,  _______,  TG(_GAME),  TG(_GAME)
+    ),
+    [_FUNC] = LAYOUT_ortho_5x12(
+        _______ , _______,    KC_F10 ,  KC_F11 ,  KC_F12 ,  _______,        _______,  _______,  _______,  _______,  _______, RESET  ,
+        _______ , _______,    KC_F7  ,  KC_F8  ,  KC_F9  ,  _______,        _______,  _______,  _______,  _______,  _______, _______,
+        _______ , KC_LGUI,    KC_F4  ,  KC_F5  ,  KC_F6  ,  _______,        _______,  KC_RSFT,  KC_RCTL,  KC_RALT,  KC_LGUI, _______,
+        _______ , _______,    KC_F1  ,  KC_F2  ,  KC_F3  ,  _______,        _______,  _______,  _______,  _______,  _______, _______,
+                  _______,    _______,  _______,  _______,  _______,        _______,  _______,  _______,  TG(_GAME),  TG(_GAME)
+    ),
 };
