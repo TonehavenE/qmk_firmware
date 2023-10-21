3fa3f51d2bed85d9f86cc21212437cbe598e0fbb
TonehavenE <tonehaven@outlook.com>
2023-10-21 14:54:45 -0400

    apollo changes

--- keyboards/apollo/keymaps/default/keymap.c
+++ keyboards/apollo/keymaps/default/keymap.c
@@ -13,19 +13,19 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_split_3x6_3(
-//    ┌─────────┬───────────┬───┬───┬─────┬─────┐               ┌──────┬─────┬───┬───┬───────────┐
-//    │ QK_GESC │     q     │ w │ f │  p  │  g  │               │  j   │  l  │ u │ y │     '     │
-//    ├─────────┼───────────┼───┼───┼─────┼─────┤               ├──────┼─────┼───┼───┼───────────┤
-//    │  lsft   │     a     │ r │ s │  t  │  d  │               │  h   │  n  │ e │ i │     o     │
-//    ├─────────┼───────────┼───┼───┼─────┼─────┤               ├──────┼─────┼───┼───┼───────────┤
-//    │  lctl   │ LALT_T(z) │ x │ c │  v  │  b  │               │  k   │  m  │ , │ . │ LGUI_T(;) │
-//    └─────────┴───────────┴───┴───┼─────┼─────┼─────┐   ┌─────┼──────┼─────┼───┴───┴───────────┘
-//                                  │ NAV │ spc │ tab │   │ ent │ lsft │ SYM │
-//                                  └─────┴─────┴─────┘   └─────┴──────┴─────┘
+//    ┌─────────┬───────────┬───┬───┬─────┬─────┐               ┌─────────┬─────┬───┬───┬───────────┐
+//    │ QK_GESC │     q     │ w │ f │  p  │  g  │               │    j    │  l  │ u │ y │     '     │
+//    ├─────────┼───────────┼───┼───┼─────┼─────┤               ├─────────┼─────┼───┼───┼───────────┤
+//    │  lsft   │     a     │ r │ s │  t  │  d  │               │    h    │  n  │ e │ i │     o     │
+//    ├─────────┼───────────┼───┼───┼─────┼─────┤               ├─────────┼─────┼───┼───┼───────────┤
+//    │  lctl   │ LALT_T(z) │ x │ c │  v  │  b  │               │    k    │  m  │ , │ . │ LGUI_T(;) │
+//    └─────────┴───────────┴───┴───┼─────┼─────┼─────┐   ┌─────┼─────────┼─────┼───┴───┴───────────┘
+//                                  │ NAV │ spc │ tab │   │ ent │ QK_LEAD │ SYM │
+//                                  └─────┴─────┴─────┘   └─────┴─────────┴─────┘
   QK_GESC , KC_Q         , KC_W , KC_F , KC_P , KC_G   ,                       KC_J    , KC_L , KC_U    , KC_Y   , KC_QUOT        ,
   KC_LSFT , KC_A         , KC_R , KC_S , KC_T , KC_D   ,                       KC_H    , KC_N , KC_E    , KC_I   , KC_O           ,
   KC_LCTL , LALT_T(KC_Z) , KC_X , KC_C , KC_V , KC_B   ,                       KC_K    , KC_M , KC_COMM , KC_DOT , LGUI_T(KC_SCLN),
-                                         NAV  , KC_SPC , KC_TAB ,     KC_ENT , KC_LSFT , SYM
+                                         NAV  , KC_SPC , KC_TAB ,     KC_ENT , QK_LEAD , SYM
 ),
 
 [_NAVIGATION] = LAYOUT_split_3x6_3(
@@ -36,7 +36,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├─────┼─────────┼──────┼──────┼───────┼──────┤               ├──────┼──────┼──────┼──────┼──────┤
 //    │     │         │ cut  │ copy │ paste │      │               │ pscr │ ins  │      │      │      │
 //    └─────┴─────────┴──────┴──────┼───────┼──────┼─────┐   ┌─────┼──────┼──────┼──────┴──────┴──────┘
-//                                  │       │      │     │   │     │      │      │
 //                                  │       │      │     │   │     │      │      │
 //                                  └───────┴──────┴─────┘   └─────┴──────┴──────┘
   KC_GRAVE , KC_MUTE , KC_MS_BTN1 , KC_MS_U , KC_MS_BTN2 , KC_MPLY ,                         KC_PGUP , KC_HOME   , KC_UP   , KC_END   , KC_DEL ,
@@ -97,7 +96,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void leader_end_user(void) {
     // Text editing
-    if (leader_sequence_two_keys(KC_D, KC_W)) {
+    if (leader_sequence_one_key(KC_ESC)) {
+        ; // nothing on esc!
+    }
+    else if (leader_sequence_two_keys(KC_D, KC_W)) {
         // delete word
         SEND_STRING(SS_LCTL(SS_TAP(X_BSPC)));
     }
@@ -149,41 +151,150 @@ void leader_end_user(void) {
         tap_code16(KC_MPRV);
     }
     // Lighting / RGB controls
-    else if (leader_sequence_two_keys(KC_L, KC_T)) {
+    else if (leader_sequence_two_keys(KC_R, KC_T)) {
         rgb_matrix_toggle();
     }
-    else if (leader_sequence_two_keys(KC_L, KC_N)) {
+    else if (leader_sequence_two_keys(KC_R, KC_N)) {
         rgb_matrix_step();
     }
-    else if (leader_sequence_two_keys(KC_L, KC_P)) {
+    else if (leader_sequence_two_keys(KC_R, KC_P)) {
         rgb_matrix_step_reverse();
     }
-    else if (leader_sequence_two_keys(KC_L, KC_U)) {
+    else if (leader_sequence_two_keys(KC_R, KC_U)) {
         rgb_matrix_increase_val();
     }
-    else if (leader_sequence_two_keys(KC_L, KC_D)) {
+    else if (leader_sequence_two_keys(KC_R, KC_D)) {
         rgb_matrix_decrease_val();
     }
     // Open Applications
     else if (leader_sequence_two_keys(KC_O, KC_C)) {
         tap_code16(KC_CALC);
     }
-    // Email
+    // Email and Phone
+    // Email/Student
     else if (leader_sequence_two_keys(KC_E, KC_S)) {
         SEND_STRING("student.equenneville@gmail.com");
     }
+    // Email/Personal
     else if (leader_sequence_two_keys(KC_E, KC_P)) {
         SEND_STRING("tonehaven@outlook.com");
     }
+    // Email/College
+    else if (leader_sequence_two_keys(KC_E, KC_C)) {
+        SEND_STRING("EQuenneville066@students.ccsnh.edu");
+    }
+    // Phone/Personal
     else if (leader_sequence_two_keys(KC_P, KC_P)) {
         SEND_STRING("(603) 484-0402");
     }
+    // Phone/Home
     else if (leader_sequence_two_keys(KC_P, KC_H)) {
         SEND_STRING("(603) 262-5730");
     }
 
-    // YAML Frontmatter
-    else if (leader_sequence_two_keys(KC_F, KC_M)) {
+    // Markdown Shortcuts
+
+    // Markdown/Template
+    else if (leader_sequence_two_keys(KC_M, KC_T)) {
+        SEND_STRING(SS_LALT("e"));
+    }
+    // Markdown/YAML Frontmatter
+    else if (leader_sequence_two_keys(KC_M, KC_Y)) {
         SEND_STRING("---" SS_TAP(X_ENTER) "tags: " SS_TAP(X_ENTER) "---" SS_TAP(X_UP) SS_TAP(X_END));
+    }
+
+    // LaTeX
+
+    // LaTeX/Symbols
+
+    // Pi
+    else if (leader_sequence_three_keys(KC_L, KC_S, KC_P)) {
+        SEND_STRING("\\pi");
+    }
+    // Delta
+    else if (leader_sequence_three_keys(KC_L, KC_S, KC_D)) {
+        SEND_STRING("\\Delta");
+    }
+    // Sigma
+    else if (leader_sequence_three_keys(KC_L, KC_S, KC_S)) {
+        SEND_STRING("\\Sigma");
+    }
+
+    // Latex/Fraction
+    else if (leader_sequence_two_keys(KC_L, KC_F)) {
+        SEND_STRING("\\frac{}{}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) );
+    }
+    // Latex/Times
+    else if (leader_sequence_two_keys(KC_L, KC_T)) {
+        SEND_STRING("\\times ");
+    }
+
+    // Latex/Units/Meter
+    else if (leader_sequence_three_keys(KC_L, KC_U, KC_M)) {
+        SEND_STRING("\\text{ m}");
+    }
+    // Latex/Units/Meter/Second
+    else if (leader_sequence_four_keys(KC_L, KC_U, KC_M, KC_S)) {
+        SEND_STRING("\\text{ m/s}");
+    }
+    // Latex/Units/Second
+    else if (leader_sequence_three_keys(KC_L, KC_U, KC_S)) {
+        SEND_STRING("\\text{ s}");
+    }
+    // Latex/Units/kg
+    else if (leader_sequence_three_keys(KC_L, KC_U, KC_K)) {
+        SEND_STRING("\\text{ kg}");
+    }
+
+    // Latex/Integral
+    else if (leader_sequence_two_keys(KC_L, KC_I)) {
+        SEND_STRING("\\int_{}^{}{ d}");
+    }
+
+    // Latex/Tex/T
+    else if (leader_sequence_two_keys(KC_T, KC_T)) {
+        SEND_STRING("\\text{}" SS_TAP(X_LEFT));
+    }
+
+    // Latex/Hat/I
+    else if (leader_sequence_three_keys(KC_L, KC_H, KC_I)) {
+        SEND_STRING("\\hat{\\textbf{i}}");
+    }
+    // Latex/Hat/J
+    else if (leader_sequence_three_keys(KC_L, KC_H, KC_J)) {
+        SEND_STRING("\\hat{\\textbf{j}}");
+    }
+    // Latex/Hat/K
+    else if (leader_sequence_three_keys(KC_L, KC_H, KC_K)) {
+        SEND_STRING("\\hat{\\textbf{K}}");
+    }
+
+    // Latex/Trig/Sin
+    else if (leader_sequence_three_keys(KC_L, KC_T, KC_S)) {
+        SEND_STRING("\\sin{}" SS_TAP(X_LEFT));
+    }
+    // Latex/Trig/Cos
+    else if (leader_sequence_three_keys(KC_L, KC_T, KC_C)) {
+        SEND_STRING("\\cos{}" SS_TAP(X_LEFT));
+    }
+    // Latex/Trig/Tan
+    else if (leader_sequence_three_keys(KC_L, KC_T, KC_T)) {
+        SEND_STRING("\\tan{}" SS_TAP(X_LEFT));
+    }
+    // Latex/Trig/Se/Cant
+    else if (leader_sequence_four_keys(KC_L, KC_T, KC_S, KC_C)) {
+        SEND_STRING("\\sec{}" SS_TAP(X_LEFT));
+    }
+    // Latex/Trig/Arc/Sin
+    else if (leader_sequence_four_keys(KC_L, KC_T, KC_A, KC_S)) {
+        SEND_STRING("\\arcsin{}" SS_TAP(X_LEFT));
+    }
+    // Latex/Trig/Arc/Cos
+    else if (leader_sequence_four_keys(KC_L, KC_T, KC_A, KC_C)) {
+        SEND_STRING("\\arccos{}" SS_TAP(X_LEFT));
+    }
+    // Latex/Trig/Arc/Tan
+    else if (leader_sequence_four_keys(KC_L, KC_T, KC_A, KC_T)) {
+        SEND_STRING("\\arctan{}" SS_TAP(X_LEFT));
     }
 }
