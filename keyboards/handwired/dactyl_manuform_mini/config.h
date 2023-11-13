43c3cd5f96b5bcab7b2e001ca70ff69b6c09ed56
TonehavenE <tonehaven@outlook.com>
2023-11-13 10:54:35 -0500

    starting to adjust dactyl keymap

--- keyboards/handwired/dactyl_manuform_mini/config.h
+++ keyboards/handwired/dactyl_manuform_mini/config.h
@@ -56,4 +56,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
 #define BOOTMAGIC_LITE_ROW_RIGHT 6
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 5 
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
--- keyboards/handwired/dactyl_manuform_mini/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_mini/keymaps/default/keymap.c
@@ -1,3 +1,4 @@
+#include "keycode.h"
 #include QMK_KEYBOARD_H
 #include "print.h"
 #define _QWERTY 0
@@ -11,18 +12,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0   , KC_EQUAL,
         KC_LALT, KC_Q  , KC_W  , KC_F  , KC_P  , KC_G  ,                         KC_J  , KC_L  , KC_U  , KC_Y  , KC_QUOT, KC_SCLN,
-        KC_LSFT, KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                         KC_H  , KC_N  , KC_E  , KC_I  , KC_O   , KC_RSFT,
-        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_K  , KC_M  ,KC_COMM,KC_DOT , KC_SLSH, KC_MINUS,
-                         KC_LBRC,KC_RBRC,                                                       KC_LPRN, KC_RPRN,
+        KC_LSFT, KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                         KC_H  , KC_N  , KC_E  , KC_I  , KC_O   , KC_BSLASH,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_K  , KC_M  ,KC_COMM,KC_DOT , KC_SLSH, LGUI_T(KC_MINUS),
+                         KC_LPRN,KC_RPRN,                                                       KC_LBRC, KC_RBRC,
                                          LOWER,                                         KC_BSPC,
                                          KC_TAB, KC_SPC,                        KC_ENT,  RAISE,
-                                         KC_LGUI, KC_TAB,                        KC_DEL,KC_BSLASH
+                                         KC_LGUI, KC_TAB,                        KC_DEL,KC_LGUI
     ),
 
     [_LOWER] = LAYOUT_5x6(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        _______,_______,KC_7, KC_8, KC_9,KC_LBRC,                        KC_RBRC, KC_HOME , KC_UP , KC_END ,_______,KC_PLUS,
-        _______,KC_HOME,KC_4, KC_5, KC_6 ,KC_LPRN,                        KC_RPRN, KC_LEFT , KC_DOWN , KC_RIGHT ,KC_MINS,KC_PIPE,
+        _______,KC_MS_WH_UP,KC_MS_BTN1, KC_MS_U, KC_MS_BTN2,KC_LBRC,                        KC_RBRC, KC_HOME , KC_UP , KC_END ,_______,KC_PLUS,
+        _______,KC_MS_WH_DOWN,KC_MS_L, KC_MS_D, KC_MS_R ,KC_LPRN,                        KC_RPRN, KC_LEFT , KC_DOWN , KC_RIGHT ,KC_MINS,KC_PIPE,
         _______,_______,KC_1, KC_2, KC_3,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                         KC_0, KC_0,                                                        _______, KC_P0,
                                                 _______,                            _______,
