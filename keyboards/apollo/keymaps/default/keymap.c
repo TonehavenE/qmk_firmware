8ddbd876b8da293ba0afd107d0a8b85c503c77bb
Eben Quenneville <tonehaven@outlook.com>
2024-06-25 16:30:53 -0400

    slight keymap adjustments

--- keyboards/apollo/keymaps/default/keymap.c
+++ keyboards/apollo/keymaps/default/keymap.c
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └─────┴─────────┴───┴───┼─────┼──────┼─────┐   ┌─────┼──────┼──────┼──────┴──────┴──────┘
 //                            │     │      │     │   │     │      │      │
 //                            └─────┴──────┴─────┘   └─────┴──────┴──────┘
-  KC_GRAVE , QK_LEAD , KC_7 , KC_8 , KC_9    , KC_MPLY ,                         KC_PGUP , KC_HOME   , KC_UP   , KC_END   , KC_DEL ,
-  _______  , KC_0    , KC_4 , KC_5 , KC_6    , KC_VOLU ,                         KC_PGDN , KC_LEFT   , KC_DOWN , KC_RIGHT , KC_BSPC,
+  KC_GRAVE , KC_ESC , KC_7 , KC_8 , KC_9    , _______ ,                         KC_PGUP , KC_HOME   , KC_UP   , KC_END   , KC_DEL ,
+  _______  , KC_0    , KC_4 , KC_5 , KC_6    , QK_LEAD ,                         KC_PGDN , KC_LEFT   , KC_DOWN , KC_RIGHT , KC_BSPC,
   _______  , _______ , KC_1 , KC_2 , KC_3    , _______ ,                         KC_PSCR , KC_INSERT , _______ , _______  , _______,
                                      _______ , _______ , _______ ,     OSM(MOD_LCTL) , _______ , _______
 ),
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └─────┴─────┴──────┴──────┼──────┼─────┼─────────┐   ┌─────────┼─────┼─────┼────┴────┴──────┘
 //                              │      │ spc │ QK_BOOT │   │ QK_BOOT │     │     │
 //                              └──────┴─────┴─────────┘   └─────────┴─────┴─────┘
-  _______ , KC_SPC  , KC_MS_BTN1 , KC_MS_U , KC_MS_BTN2 , KC_F12 ,                         KC_F6   , KC_F7   , KC_F8   , KC_F9  , KC_BSPC,
-  _______ , KC_0    , KC_MS_L    , KC_MS_D , KC_MS_R    , KC_F11 ,                         KC_F1   , KC_F2   , KC_F3   , KC_F4  , KC_F5  ,
+  _______ , KC_WH_U  , KC_MS_BTN1 , KC_MS_U , KC_MS_BTN2 , KC_F12 ,                         KC_F6   , KC_F7   , KC_F8   , KC_F9  , KC_BSPC,
+  _______ , KC_WH_D    , KC_MS_L    , KC_MS_D , KC_MS_R    , KC_F11 ,                         KC_F1   , KC_F2   , KC_F3   , KC_F4  , KC_F5  ,
   _______ , KC_PLUS , KC_1       , KC_2    , KC_3       , KC_F10 ,                         KC_F12  , KC_EQL  , KC_COMM , KC_DOT , KC_MINS,
                                              _______    , KC_SPC , QK_BOOT ,     QK_BOOT , _______ , _______
 )
